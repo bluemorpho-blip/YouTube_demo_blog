895290c2e0c8ccceac75e3b7f9f18cdf4c348328
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-04 13:46:50 -0500

    post controller editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [:show, :update, :edit]
+
+
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
@@ -18,11 +22,26 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
+  def show; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
+  def edit
+
+  end
+
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
